a5580545eff7d5570b246491b920a4fdc1dd73b8
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-04-22 14:30:04 -0500

    started working on questions array and license functions

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const fs = require('fs')
 const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  'What would you like the title of your README file to be?',
+  'What would you like the description to be?',
+  'Describe the installation process for this project.',
+  'Describe the functionality/usage of this project.',
+  'Please type the names of any contributors to this project.',
+  'Please choose a license for this project from this list.',
+  'Please provide your GitHub username so people can see your GitHub profile.',
+  'Please enter your email address so people can contact you with any questions.',
+  
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const fileName = `README.md`;
+  fs.writeFile(fileName, data, null, (error) =>
+  error ? console.error(error) : console.log("success"));
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,61 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license.answer == true ){
+
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let renderLicence = 
+  `
+  ## License
+  `
+  return renderLicense;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(data, renderLicence) {
+  let readMe = 
+  `# ${data.title}
 
-`;
+  ## Description
+
+    * ${data.description}
+  
+  ## Table of Contents (Optional)
+    
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  
+  ## Installation
+  
+  
+
+  ## Usage
+  
+  
+
+  ## Contributing
+
+  
+
+  ## Tests
+
+  ${renderLicense}
+
+  `;
+  return readMe
 }
 
 module.exports = generateMarkdown;
